fb61281f4ab805004ce57a640c937c6fe71c8f58
DsDravos-lnx <ds.hallucinate@gmail.com>
2020-02-12 21:13:35 -0300

    Diagonal

--- script/script.js
+++ script/script.js
@@ -7,5 +7,15 @@
     liElements[0].style.color = '#00f'
     liElements[2].style.fontWeight = 'bold'
     ulElement[0].style.border = 'solid'
-    
+
+    const diagonal = function(){
+        let trElements = document.getElementsByTagName('tr')
+        let tdElements = document.getElementsByTagName('td')
+        for(let i=0; i<3; i++) {
+            for(let c=0; c<3; c++){
+                
+            }
+        }
+    }
+
 })()
