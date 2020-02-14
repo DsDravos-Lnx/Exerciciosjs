edaa99f237bbdc87873d91389e787be785c3dd52
DsDravos-lnx <ds.hallucinate@gmail.com>
2020-02-14 19:55:15 -0300

    matriz

--- script/script.js
+++ script/script.js
@@ -3,16 +3,63 @@
     const liElements = document.getElementsByTagName('li')
     const ulElement = document.getElementsByTagName('ul')
 
+    // 1 - a
     liElements[0].style.color = '#00f'
-    liElements[2].style.fontWeight = 'bold'
+    
+    // 1 - b
+
+    for (i=0; i<3; i++){
+        liElements[i].style.fontWeight = 'bold'
+    }    
+    
+    // 1 - c
+
     ulElement[0].style.border = 'solid'
 
-    const diagonal = function(){
+    // 1 - d
+
+    ulElement[0].previousElementSibling.style.color = 'red'
+
+    // 2
+    
+    const createTable = function(){
+        let table = "<table border='1'>"
+        for(let i=0; i<3; i++){
+            table += '<tr>'
+            for(let c=0; c<3; c++){
+                table += '<td>element'+(i+1)+'</td>'
+            }
+            table += '</tr>'
+        }
+        table += '</table>'
+
+        document.body.innerHTML = table
+    }
+    
+    // 3
+
+    const primaryDiagonal = function(){
         let trElements = document.getElementsByTagName('tr')
         for(let i=0; i<3; i++) {
             trElements[i].getElementsByTagName('td')[i].style.color = '#ffa500'       
         }
     }
-    diagonal()
+    
+    // 4
+
+    const secondaryDiagonal = function(){
+        let trElements = document.getElementsByTagName('tr')
+        for(let i=0; i<3; i++){                
+                trElements[(2-i)].getElementsByTagName('td')[i].style.color = '#00f'       
+            }
+    }
+    
+    // const selectFunction = function(){
+    //     if(value == 1){
+    //         primaryDiagonal()
+    //     }else{
+    //         secondaryDiagonal()
+    //     }
+    // }
 
 })()
