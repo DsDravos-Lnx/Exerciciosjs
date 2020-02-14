048ebbc7924ddcce7254788e7b2cdb783a4e99b6
DsDravos-lnx <ds.hallucinate@gmail.com>
2020-02-14 10:43:03 -0300

    Matriz

--- script/script.js
+++ script/script.js
@@ -3,19 +3,16 @@
     const liElements = document.getElementsByTagName('li')
     const ulElement = document.getElementsByTagName('ul')
 
-
     liElements[0].style.color = '#00f'
     liElements[2].style.fontWeight = 'bold'
     ulElement[0].style.border = 'solid'
 
     const diagonal = function(){
         let trElements = document.getElementsByTagName('tr')
-        let tdElements = document.getElementsByTagName('td')
         for(let i=0; i<3; i++) {
-            for(let c=0; c<3; c++){
-                
-            }
+            trElements[i].getElementsByTagName('td')[i].style.color = '#ffa500'       
         }
     }
+    diagonal()
 
 })()
